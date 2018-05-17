d466c134a4baa2af6c29e10a08008354af31f2bc
Ondřej Hlavatý <aearsis@eideo.cz>
2018-05-17 19:02:38 +0200

    Store label for contact

--- app/src/main/java/cz/mff/mobapp/database/DaoMapperFactory.java
+++ app/src/main/java/cz/mff/mobapp/database/DaoMapperFactory.java
@@ -2,6 +2,7 @@ package cz.mff.mobapp.database;
 
 import android.support.annotation.NonNull;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import cz.mff.mobapp.model.Contact;
@@ -14,13 +15,24 @@ public class DaoMapperFactory {
             public void convertToDao(@NonNull Contact from, @NonNull ContactData to) {
                 to.setId(from.getId());
                 to.setLastModified(from.getLastModified());
+
+                JSONObject jsonData = new JSONObject();
+                try {
+                    jsonData.putOpt("label", from.getLabel());
+                } catch (JSONException ignored) {
+                    // Doesn't happen, cause we create from empty object
+                }
+                to.setData(jsonData);
             }
 
             @Override
             public void convertFromDao(@NonNull ContactData from, @NonNull Contact to) {
                 to.setId(from.getId());
                 to.setLastModified(from.getLastModified());
-                to.setContact(true);
+                JSONObject jsonObject = from.getData();
+
+                String label = jsonObject.optString("label");
+                if (label != null) to.setLabel(label);
             }
 
             @NonNull
